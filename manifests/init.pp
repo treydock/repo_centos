15a9e019cc068548064594a4f5ce9a14125356e0
Mike Hanby <mhanby@uab.edu>
2014-02-05 12:42:59 -0600

    Added new CentOS repository SCL

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,21 +13,25 @@ class repo_centos (
   $enable_plus = false,
   $enable_cr = false,
   $enable_extras = false,
+  $enable_scl = false,
 ) inherits repo_centos::params {
 
   if $::operatingsystem == 'CentOS' {
     include repo_centos::base
     include repo_centos::updates
     class { "repo_centos::cr":
-      enable_cr   => $enable_cr,
+      enable_cr       => $enable_cr,
     }
     class { "repo_centos::plus":
-      enable_plus   => $enable_plus,
+      enable_plus     => $enable_plus,
     }
     class { "repo_centos::extras":
       enable_extras   => $enable_extras,
     }
-
+    class { "repo_centos::scl":
+      enable_scl      => $enable_scl,
+    }
+    
     repo_centos::rpm_gpg_key{ "RPM-GPG-KEY-CentOS-${::os_maj_version}":
       path => "/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-${::os_maj_version}",
     }
