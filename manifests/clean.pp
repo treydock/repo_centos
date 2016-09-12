ef2d6623adf490fa7255916b293f19089f4bc7a6
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2016-09-12 13:46:54 +0100

    Clean up old lowercase files from earlier versions of this module

--- manifests/clean.pp
+++ manifests/clean.pp
@@ -2,6 +2,21 @@
 #
 class repo_centos::clean {
 
-  file { "/etc/yum.repos.d/centos${repo_centos::releasever}.repo": ensure => absent }
+  file { [
+    "/etc/yum.repos.d/centos${repo_centos::releasever}.repo",
+    '/etc/yum.repos.d/centos-base.repo',
+    '/etc/yum.repos.d/centos-base-source.repo',
+    '/etc/yum.repos.d/centos-contrib.repo',
+    '/etc/yum.repos.d/centos-cr.repo',
+    '/etc/yum.repos.d/centos-debug.repo',
+    '/etc/yum.repos.d/centos-extras.repo',
+    '/etc/yum.repos.d/centos-fasttrack.repo',
+    '/etc/yum.repos.d/centos-plus.repo',
+    '/etc/yum.repos.d/centos-scl.repo',
+    '/etc/yum.repos.d/centos-updates.repo',
+    '/etc/yum.repos.d/centos-updates-source.repo',
+  ]:
+    ensure => absent,
+  }
 
 }
